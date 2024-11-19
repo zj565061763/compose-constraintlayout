433d9bb3ddcdb4de7c4e313e57e9ef713e84d1c4
zjun <565061763@qq.com>
2024-11-20 00:36:58 +0800

    sample

--- app/src/main/java/com/sd/demo/compose/constraintlayout/SampleLine.kt
+++ app/src/main/java/com/sd/demo/compose/constraintlayout/SampleLine.kt
@@ -3,13 +3,16 @@ package com.sd.demo.compose.constraintlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.sd.demo.compose.constraintlayout.theme.AppTheme
 import com.sd.lib.compose.constraintlayout.fHorizontalLine
 import com.sd.lib.compose.constraintlayout.fVerticalLine
@@ -32,19 +35,21 @@ private fun Content(
    ConstraintLayout(
       modifier = modifier.fillMaxSize(),
    ) {
-      val hLine = fHorizontalLine(bias = 0.5f, target = null)
-      val vLine = fVerticalLine(bias = 0.5f, target = null)
-
-      val refText = createRef()
-
-      Text(
-         "text",
-         fontSize = 36.sp,
-         modifier = Modifier.constrainAs(refText) {
-            centerHorizontallyTo(vLine)
-            centerVerticallyTo(hLine)
-         },
+      val refBox = createRef()
+      Box(
+         modifier = Modifier
+            .background(Color.Blue)
+            .constrainAs(refBox) {
+               width = Dimension.value(100.dp)
+               height = Dimension.value(100.dp)
+            },
       )
+
+      fHorizontalLine(bias = 0.5f, target = null)
+      fVerticalLine(bias = 0.5f, target = null)
+
+      fHorizontalLine(bias = 0.5f, target = refBox, color = Color.Green)
+      fVerticalLine(bias = 0.5f, target = refBox, color = Color.Green)
    }
 }
 
